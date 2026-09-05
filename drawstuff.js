cd14e1a869f698a196c9a68acaede596024e4625
toxicfaIIs <apolloalexf@gmail.com>
2026-09-05 18:58:38 -0400

    Commit to pages build.

--- drawstuff.js
+++ drawstuff.js
@@ -518,7 +518,7 @@ function main() {
     // define polygon and view
     var testEye = new Vector(0,0,0);
     var testAt = Vector.subtract(new Vector(0,0,10),testEye);
-    var view = {eye:testEye, at:testAt, up:new Vector(0,1,0)};
+    var view = {eye:testEye, at:testAt, up:new Vector(1,1,0)};
     var poly = [{x:-5,y:5,z:10,c:new Color(255,0,0,255)}, {x:5,y:5,z:10,c:new Color(0,255,0,255)}, 
                 {x:5,y:-5,z:10,c:new Color(0,0,0,255)}, {x:-5,y:-5,z:10,c:new Color(0,0,255,255)}];
     
